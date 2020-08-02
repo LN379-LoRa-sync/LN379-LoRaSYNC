f5bc65b520c35172cd0f6d5b12e1391ec4eb3141
Gaurav-Mishra-ji <58990909+Gaurav-Mishra-ji@users.noreply.github.com>
2020-08-02 09:39:29 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE db1;
+USE db1;
+CREATE TABLE mydata(id varchar(10), name varchar(10), water_level double, last_updated timestamp);
+INSERT INTO mydata('id','name',water_level);
+SELECT*FROM mydata;
