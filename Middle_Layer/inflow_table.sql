3a48fe11f67d7c410e2a27f9cbf0d372e82b7744
Gaurav-Mishra-ji <58990909+Gaurav-Mishra-ji@users.noreply.github.com>
2020-08-03 13:41:50 +0530

    Create inflow_table.sql

---
+++ Middle_Layer/inflow_table.sql
@@ -0,0 +1,7 @@
+
+CREATE DATABASE db1;
+USE db1;
+CREATE TABLE mydata(id varchar(10), name varchar(10), water_level double, last_updated timestamp);
+INSERT INTO mydata(id,name,water_level) VALUES ('id_values','name',water_level);
+SELECT*FROM mydata;
+>>>>>>> 15a041117f13ace018ab16bc5ee2297d3e8b2719:database.sql
