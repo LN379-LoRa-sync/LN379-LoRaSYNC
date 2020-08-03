238de0b2a32975f09ca9679fe2882f6887a06aa7
Gaurav-Mishra-ji <58990909+Gaurav-Mishra-ji@users.noreply.github.com>
2020-08-03 13:41:57 +0530

    Update database.sql

--- Middle_Layer/database.sql
+++ Middle_Layer/database.sql
@@ -1,7 +1,16 @@
-
+#All data is stored in mysql databses;
 CREATE DATABASE db1;
 USE db1;
+#Table for water collection in reservoir
 CREATE TABLE mydata(id varchar(10), name varchar(10), water_level double, last_updated timestamp);
 INSERT INTO mydata(id,name,water_level) VALUES ('id_values','name',water_level);
 SELECT*FROM mydata;
->>>>>>> 15a041117f13ace018ab16bc5ee2297d3e8b2719:database.sql
+
+#Table for all whether data station
+USE db1;
+CREATE TABLE all_whether_station(temp double,humidity double, solar_raditon double,
+ pressure double, evaporation double, windspeed double, last_updated timestamp);
+INSERT INTO all_whether_station(id,name,water_level) VALUES ('id_values','name',water_level);
+SELECT*FROM all_whether_station;
+#databses for net reservoir level,canal level and precipitation
+
